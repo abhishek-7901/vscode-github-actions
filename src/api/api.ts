4f63d317342cbfcb9acbd6a3993309886fd4ded3
K Abhishek <konthalapalli070901@gmail.com>
2024-03-05 20:10:22 +0530

    add request

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,31 +1,30 @@
-import {Octokit} from "@octokit/rest";
-import {version} from "../../package.json";
-import {getGitHubApiUri} from "../configuration/configuration";
+import { Octokit } from "@octokit/rest";
+import { version } from "../../package.json";
+import { getGitHubApiUri } from "../configuration/configuration";
 
-import {fetch as undiciFetch, ProxyAgent} from "undici" ; 
+import { fetch as undiciFetch, ProxyAgent } from "undici";
 
 
 export const userAgent = `VS Code GitHub Actions (${version})`;
 
-
-
-
 export function getClient(token: string): Octokit {
-  const proxyUrl = `${process.env.HTTP_PROXY}$`; 
+  const proxyUrl = `${process.env.HTTP_PROXY}$`;
 
-  console.log("proxy url is" + proxyUrl) ; 
+  console.log("proxy url is" + proxyUrl);
 
   const customFetchFunction = (url: string, options: any) => {
     return undiciFetch(url, {
-      ...options, 
-      dispatcher: new ProxyAgent(proxyUrl),  
+      ...options,
+      dispatcher: new ProxyAgent(proxyUrl),
     });
-  }; 
+  };
 
   return new Octokit({
     auth: token,
     userAgent: userAgent,
     baseUrl: getGitHubApiUri(),
-    fetch : customFetchFunction
+    request: {
+      fetch: customFetchFunction
+    }
   });
 }
